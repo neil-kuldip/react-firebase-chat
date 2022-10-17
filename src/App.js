4846ed2eb056b0c805eef875918b6eb6acfd7323
neil-kuldip <neilkuldip2425@gmail.com>
2022-10-17 15:56:49 -0400

    init: create firebase auth, storage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZLntCg1pT3qLqRdJ8sRguBNfJLsTICjw",
+  authDomain: "superchat-f998e.firebaseapp.com",
+  projectId: "superchat-f998e",
+  storageBucket: "superchat-f998e.appspot.com",
+  messagingSenderId: "157107663270",
+  appId: "1:157107663270:web:da38822689256dc9895f67",
+  measurementId: "G-YL1B7L87L7"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = getAnalytics(app);
 
 function App() {
+
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          {user ? <ChatRoom/> : <SignIn/>}
+        </section>
       </header>
     </div>
   );
